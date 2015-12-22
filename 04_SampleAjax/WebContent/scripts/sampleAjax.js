87b27df4f6d7d13d314ab821f6e1541724fce1fe
IvoSlavchev <ivelin.slavchev97@gmail.com>
2015-12-22 10:29:28 +0200

    Adding code from class

---
+++ 04_SampleAjax/WebContent/scripts/sampleAjax.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	"use strict";
+	var ENDPOINT = "http://localhost:3000/tasks";
+	function taskEndpoint(taskId) {
+		return ENDPOINT + "/" + taskId;
+	}
+
+	$(document).ajaxError(function() {
+		console.log("error: ", arguments);
+		alert("Error!");
+	});
+
+	$.ajax(ENDPOINT, {
+		method: "GET",
+		data: {
+			title: "hello"
+		},
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+	});
+
+	$.ajax(taskEndpoint(1), {
+		method: "GET",
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+	});
+
+	var task = {
+		title: "hello",
+		description: "some text"
+	};
+	var createPromise = $.ajax(ENDPOINT, {
+		method: "POST",
+		contentType: "application/json; charset=utf-8",
+		data: JSON.stringify(task),
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+		return response;
+	});
+
+	$.ajax(taskEndpoint(2), {
+		method: "PUT",
+		contentType: "application/json; charset=utf-8",
+		data: JSON.stringify({
+			title: "updated",
+			description: "new description"
+		}),
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+	});
+
+	createPromise.then(function(response) {
+		$.ajax(taskEndpoint(response.id), {
+			method: "DELETE"
+		});
+	});
+});
